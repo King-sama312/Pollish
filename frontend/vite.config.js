edb9d125b8e14159e82cea0472151179427d984f
King sama <211346423+King-sama312@users.noreply.github.com>
2026-05-16 00:23:24 +0530

    feat: configure vite proxy to use environment variable for backend URL

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'http://localhost:5000'
-    }
-  },
-  plugins: [tailwindcss(), react()],
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    server: {
+      proxy: {
+        '/api': env.BACKEND_URL || 'http://localhost:5000'
+      }
+    },
+    plugins: [tailwindcss(), react()],
+  }
 })
